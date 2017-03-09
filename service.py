dbc8c6142fe7a1b8f5e1a39fb23bae9ba34d566d
Greg Valentin <gregvary@users.noreply.github.com>
2017-03-09 10:14:46 +0100

    updated images and added commands

--- service.py
+++ service.py
@@ -14,10 +14,14 @@ def hello_world():
     message = req_content['item']['message']['message'].lower()
     # mentions = req_content['item']['message']['mentions'].lower()
     if 'dataman' in message:
-        return jsonify({"color": "red", "message": "http://i.imgur.com/xZtBOsq.jpg", "notify": True,"message_format": "text"}),201
+        return jsonify({"color": "red", "message": "http://i.imgur.com/LX8JyTC.jpg", "notify": True,"message_format": "text"}),201
     if 'volkskaffe' in message:
         return jsonify({"color": "red", "message": "http://i.imgur.com/nwmsi10.jpg", "notify": True, "message_format": "text"}),201
-    return jsonify({"color": "red", "message": "Usage: /gerg (dataman|volkskaffee)", "notify": False,"message_format": "text"}),201
+    if 'deal' in message:
+        return jsonify({"color": "red", "message": "http://i.imgur.com/JEVTgfS.gifv", "notify": True, "message_format": "text"}),201
+    if 'kebab' in message:
+        return jsonify({"color": "red", "message": "https://i.imgur.com/gPCKNum.jpg", "notify": True, "message_format": "text"}),201
+    return jsonify({"color": "red", "message": "Usage: /gerg (dataman|volkskaffee|deal|kebab)", "notify": False,"message_format": "text"}),201
 
 
 #if __name__ == '__main__':
