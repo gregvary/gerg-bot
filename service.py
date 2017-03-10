036e62ef69b27b4e97a5f451d7e1a15cb74c2f3a
Greg Valentin <gregvary@users.noreply.github.com>
2017-03-10 13:08:21 +0100

    Added merlin command

--- service.py
+++ service.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify
-
+from random import randint
 
 app = Flask(__name__)
-
+quotes = ['Ich behandel meinen Körper wie einen Tempel', 'Gewöhn dich dran', 'Dein Handy ist schon ganz schön dreckig']
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
@@ -19,12 +19,14 @@ def hello_world():
         return jsonify({"color": "red", "message": "http://i.imgur.com/nwmsi10.jpg", "notify": True, "message_format": "text"}),201
     if 'deal' in message:
         return jsonify({"color": "red", "message": "http://i.imgur.com/JEVTgfS.gifv", "notify": True, "message_format": "text"}),201
-    if 'kebab' in message:
+    if 'kebab' in message or 'kebap' in message:
         return jsonify({"color": "red", "message": "https://i.imgur.com/gPCKNum.jpg", "notify": True, "message_format": "text"}),201
     if 'vong' in message:
         return jsonify({"color": "red", "message": "http://i.imgur.com/p0RqWqL.png", "notify": True, "message_format": "text"}),201
+    if 'merlin' in message:
+        return jsonify({"color": "red", "message": quotes[randint(0,len(quotes))], "notify": True, "message_format": "text"}),201
     
-    return jsonify({"color": "red", "message": "Usage: /gerg (dataman|volkskaffee|deal|kebab|vong)", "notify": False,"message_format": "text"}),201
+    return jsonify({"color": "red", "message": "Usage: /gerg (dataman|volkskaffee|deal|kebab|vong|merlin)", "notify": False,"message_format": "text"}),201
 
 
 #if __name__ == '__main__':
